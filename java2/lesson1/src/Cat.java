d541e23d03a870f5a366703a27aebe506162893e
Henctv <1@bulyak.ru>
2017-04-15 16:30:01 +0500

    Add files via upload

---
+++ java2/lesson1/src/Cat.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Администратор on 15.04.2017.
+ */
+public class Cat {
+    private String name;
+    private String color;
+    private int age;
+
+    public Cat(String name, String color, int age) {
+        this.name = name;
+        this.color = color;
+        this.age = age;
+
+    }
+}
---
+++ java2/lesson1/src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Администратор on 15.04.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
